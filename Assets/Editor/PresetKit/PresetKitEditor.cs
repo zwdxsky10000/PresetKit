48317bb92e80481d3b50521be1c38011d148f64a
toyqiu <toyqiu@tencent.com>
2021-07-07 17:35:33 +0800

    #modify PresetKitEditor

--- Assets/Editor/PresetKit/PresetKitEditor.cs
+++ Assets/Editor/PresetKit/PresetKitEditor.cs
@@ -4,6 +4,7 @@ using UnityEditor.Presets;
 using UnityEngine;
 using System.IO;
 using UnityEditor.IMGUI.Controls;
+using System;
 
 namespace PresetKit
 {
@@ -12,7 +13,7 @@ namespace PresetKit
         [MenuItem("Assets/Create/Custom/Preset", false, 0)]
         static void CreatePreset()
         {
-            Object obj = Selection.activeObject;
+            UnityEngine.Object obj = Selection.activeObject;
             string path = AssetDatabase.GetAssetPath(obj);
 
             AssetImporter importer = AssetImporter.GetAtPath(path);
@@ -34,7 +35,7 @@ namespace PresetKit
         [MenuItem("Assets/Create/Custom/PresetRule", false, 0)]
         static void CreatePresetRule()
         {
-            Object obj = Selection.activeObject;
+            UnityEngine.Object obj = Selection.activeObject;
 
             string dir = AssetDatabase.GetAssetPath(obj);//folder
 
@@ -67,6 +68,7 @@ namespace PresetKit
         [SerializeField] 
         TreeViewState m_TreeViewState;
         RuleTreeView m_RuleTreeView;
+        PresetObject m_selectPresetObj;
 
         private void OnEnable()
         {
@@ -74,6 +76,10 @@ namespace PresetKit
                 m_TreeViewState = new TreeViewState();
 
             m_RuleTreeView = new RuleTreeView(m_TreeViewState);
+            m_RuleTreeView.clickedItemCallback += (obj) =>
+            {
+                m_selectPresetObj = obj;
+            };
         }
 
         private void OnGUI()
@@ -95,14 +101,14 @@ namespace PresetKit
         private void DrawDetail()
         {
             Rect rect = new Rect(130, 5, position.width - 135, position.height-5);
-            if(true)
+            if(m_selectPresetObj == null)
             {
                 rect.height = 30f;
                 DrawEmpty(rect);
             }
             else
             {
-                DrawRuleDetail(null);
+                DrawRule(m_selectPresetObj, rect);
             }
         }
 
@@ -112,46 +118,52 @@ namespace PresetKit
         }
 
         bool foldout = false;
-        private void DrawRuleDetail(PresetObject rule)
+        private void DrawRule(PresetObject rule, Rect rect)
         {
-            EditorGUILayout.BeginVertical("box");
+            EditorGUI.DrawRect(rect, Color.black);
 
-            EditorGUILayout.BeginHorizontal("box");
-            EditorGUILayout.LabelField("Path:", GUILayout.Width(30));
-            GUI.enabled = false;
-            EditorGUILayout.TextField(rule.path);
-            GUI.enabled = true;
-            EditorGUILayout.EndHorizontal();
-
-            //折叠的相关控件 返回bool类型表示开、关
-            foldout = EditorGUILayout.Foldout(foldout, "Rules");
-            if (foldout)
+            Rect pathRect = new Rect(rect)
             {
-                foreach (var preset in rule.rules)
-                {
-                    DrawPreset(preset);
-                }
-            }
+                height = 20
+            };
+            EditorGUI.TextField(pathRect, "Path:", rule.path);
 
-            Color origin = GUI.color;
-            GUI.color = Color.green;
-            if (GUILayout.Button("Reimport"))
-            {
-                string[] files = Directory.GetFiles(rule.path);
-                if(files != null && files.Length > 0)
-                {
-                    foreach(string file in files)
-                    {
-                        if (file.Contains("PresetRule") || file.Contains(".meta"))
-                        {
-                            continue;
-                        }
-                        AssetDatabase.ImportAsset(file, ImportAssetOptions.DontDownloadFromCacheServer | ImportAssetOptions.ImportRecursive);
-                    }
-                }
-            }
-            GUI.color = origin;
-            EditorGUILayout.EndVertical();
+            //EditorGUILayout.BeginHorizontal("box");
+            //EditorGUILayout.LabelField("Path:", GUILayout.Width(30));
+            //GUI.enabled = false;
+            //EditorGUILayout.TextField(rule.path);
+            //GUI.enabled = true;
+            //EditorGUILayout.EndHorizontal();
+
+            ////折叠的相关控件 返回bool类型表示开、关
+            //foldout = EditorGUILayout.Foldout(foldout, "Rules");
+            //if (foldout)
+            //{
+            //    foreach (var preset in rule.rules)
+            //    {
+            //        DrawPreset(preset);
+            //    }
+            //}
+
+            //Color origin = GUI.color;
+            //GUI.color = Color.green;
+            //if (GUILayout.Button("Reimport"))
+            //{
+            //    string[] files = Directory.GetFiles(rule.path);
+            //    if(files != null && files.Length > 0)
+            //    {
+            //        foreach(string file in files)
+            //        {
+            //            if (file.Contains("PresetRule") || file.Contains(".meta"))
+            //            {
+            //                continue;
+            //            }
+            //            AssetDatabase.ImportAsset(file, ImportAssetOptions.DontDownloadFromCacheServer | ImportAssetOptions.ImportRecursive);
+            //        }
+            //    }
+            //}
+            //GUI.color = origin;
+            //EditorGUILayout.EndVertical();
         }
 
         private void DrawPreset(PresetRule rule)
@@ -165,24 +177,14 @@ namespace PresetKit
             rule.pattern = EditorGUILayout.TextField(rule.pattern, GUILayout.Width(70));
             EditorGUILayout.EndHorizontal();
         }
-
-        private List<PresetObject> GetPresetRules()
-        {
-            List<PresetObject> rules = new List<PresetObject>();
-            string[] guids = AssetDatabase.FindAssets("t:PresetObject");
-            foreach (var guid in guids)
-            {
-                string path = AssetDatabase.GUIDToAssetPath(guid);
-                PresetObject rule = AssetDatabase.LoadAssetAtPath<PresetObject>(path);
-                rules.Add(rule);
-            }
-            return rules;
-        }
     }
 
     class RuleTreeView : TreeView
     {
         SearchField m_searchField;
+        List<PresetObject> m_RuleObjects;
+
+        public Action<PresetObject> clickedItemCallback;
 
         public RuleTreeView(TreeViewState treeViewState)
             : base(treeViewState)
@@ -191,6 +193,7 @@ namespace PresetKit
             // 对TreeView进行一些设置
             rowHeight = 20;
             showBorder = true;
+            m_RuleObjects = GetPresetRules();
 
             Reload();
         }
@@ -209,17 +212,31 @@ namespace PresetKit
         {
             var root = new TreeViewItem { id = 0, depth = -1, displayName = "Root" };
 
-            var ruleObjects = GetPresetRules();
+            
             var allItems = new List<TreeViewItem>();
-            for (int i = 0, len = ruleObjects.Count; i < len; ++i)
+            if(m_RuleObjects != null)
             {
-                var item = new TreeViewItem { id = i + 1, depth = 0, displayName = ruleObjects[i].name };
-                allItems.Add(item);
+                for (int i = 0, len = m_RuleObjects.Count; i < len; ++i)
+                {
+                    var item = new TreeViewItem { id = i + 1, depth = 0, displayName = m_RuleObjects[i].name };
+                    allItems.Add(item);
+                }
             }
 
             SetupParentsAndChildrenFromDepths(root, allItems);
 
             return root;
+        }
+
+        protected override void SingleClickedItem(int id)
+        {
+            base.SingleClickedItem(id);
+            int index = id - 1;
+            if(m_RuleObjects != null && m_RuleObjects.Count > index && index >= 0)
+            {
+                clickedItemCallback?.Invoke(m_RuleObjects[index]);
+            }
+            Debug.Log("Clicled index:" + id);
         }
 
         private List<PresetObject> GetPresetRules()
@@ -235,6 +252,5 @@ namespace PresetKit
             return rules;
         }
     }
-
 }
 
